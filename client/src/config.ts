a0d004b5c9ad672f505e719a8f834b41a6eb8b0c
Irie Aoi <eai@mizle.net>
2021-06-27 11:56:10 +0900

    chore: .envを移動

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,10 @@
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: `${__dirname}/../../.env` });
 
 export const config = {
   token: process.env.DISCORD_TOKEN,
   timezone: process.env.TZ,
-  threshold_for_time_spent: Number(process.env.THRESHOLD_FOR_TIME_SPENT || 180000),
+  threshold_for_time_spent: Number(
+    process.env.THRESHOLD_FOR_TIME_SPENT || 180000
+  ),
 };
