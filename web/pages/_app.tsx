3aa1358df8a9dca4dae17aa6412132dfe4ecb193
Irie Aoi <eai@mizle.net>
2021-07-02 02:49:52 +0900

    fix(web): SWRのrevalidateOnFocusをデフォルト無効にする

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,11 +3,18 @@ import "../styles/globals.css";
 import "../styles/tailwind-util.css";
 import type { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
+import useSWR, { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          revalidateOnFocus: false,
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </Provider>
   );
 }
