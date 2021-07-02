6490e05ac8b277c47dd179244c7704770594e0aa
Irie Aoi <eai@mizle.net>
2021-07-02 17:57:52 +0900

    fix(web): デフォルトアバターの表示に対応する

--- web/components/Icon.tsx
+++ web/components/Icon.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 
 export const Icon: React.VFC<{ user: DiscordUser }> = ({ user }) => {
+  const avatarUrl = (() => {
+    if (user.avatar) {
+      return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+    }
+    const defaultAvatarNumber = parseInt(user.discriminator) % 5;
+    return `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+  })();
+
   return (
     <Image
       className="rounded-full"
-      src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+      src={avatarUrl}
       alt={`${user.username}'s avatar`}
       width={32}
       height={32}
